245e5af3727cc605885cefefaf9aa3f845d960b5
Muhammad Irfan <mrfansi@outlook.com>
2022-05-11 20:14:41 +0700

    Update

--- app/src/main/java/com/android/support/ActivityMain.java
+++ app/src/main/java/com/android/support/ActivityMain.java
@@ -4,10 +4,10 @@ import android.app.*;
 import android.os.*;
 
 public class ActivityMain extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        Main.Start(this);
-    }
+    Main.Start(this);
+  }
 }
--- app/src/main/java/com/mod/AppTest.java
+++ app/src/main/java/com/mod/AppTest.java
@@ -5,27 +5,27 @@ import android.content.Context;
 
 /* loaded from: classes.dex */
 public class AppTest extends Application {
-    private static AppTest instance;
+  private static AppTest instance;
 
-    public AppTest() {
-        instance = this;
-    }
+  public AppTest() {
+    instance = this;
+  }
 
 
-    public static AppTest getInstance() {
-        return instance;
-    }
+  public static AppTest getInstance() {
+    return instance;
+  }
 
-    /* JADX INFO: Access modifiers changed from: protected */
-    @Override // android.content.ContextWrapper
-    public void attachBaseContext(Context context) {
-        super.attachBaseContext(context);
-    }
+  /* JADX INFO: Access modifiers changed from: protected */
+  @Override // android.content.ContextWrapper
+  public void attachBaseContext(Context context) {
+    super.attachBaseContext(context);
+  }
 
-    @Override // android.app.Application
-    public void onCreate() {
-        //Main.Start(this);
+  @Override // android.app.Application
+  public void onCreate() {
+    //Main.Start(this);
 
-        super.onCreate();
-    }
+    super.onCreate();
+  }
 }
